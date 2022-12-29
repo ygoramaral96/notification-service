8d1bfc50700f69825d3c1b08b0753e42c6c0235d
Ygor Amaral <ygor_amaral96@hotmail.com>
2022-12-29 16:52:58 -0300

    feat: add initial files of the project

---
+++ src/app.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Get, Post } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { randomUUID } from 'node:crypto';
+
+@Controller('notifications')
+export class AppController {
+  constructor(private readonly prisma: PrismaService) {}
+
+  @Get()
+  list() {
+    return this.prisma.notification.findMany();
+  }
+
+  @Post()
+  async create() {
+    await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content: 'Você tem uma nova solicitação de amizade!',
+        category: 'social',
+        recipientId: randomUUID(),
+      },
+    });
+  }
+}
